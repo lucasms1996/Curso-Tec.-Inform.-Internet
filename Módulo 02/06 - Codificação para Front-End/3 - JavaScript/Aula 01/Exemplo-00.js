54e1b37add8629141e649b46d8773fa8d4395060
Lucas Monteiro <146959306+lucasms1996@users.noreply.github.com>
2024-01-10 11:35:43 -0300

    update

---
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 01/Exemplo-00.js
@@ -0,0 +1,10 @@
+
+function coletarDados(){
+    let nome = prompt("digite seu nome")
+    let idade = prompt("digite sua idade")
+    let altura = prompt("digite sua altura")
+    document.getElementById("nome").textContent = nome
+    document.getElementById("idade").textContent = idade
+    document.getElementById("altura").textContent = altura
+
+}
---
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Aula 01/Exemplo-02.js
@@ -0,0 +1,13 @@
+
+function alterarTXT(){
+    let lemento7 = document.getElementById("Exemplo02")
+    lemento7.textContent = "Bem Vindo 2024"
+    //lemento7.style.color = "red"
+}
+function alteracor(){
+    let alteracor = document.getElementById("alteracor")
+    alteracor.style.color = "blue"
+
+}
+
+
